99639b76e03cae5f5600b485dd6fd187358f7085
ethanhuang03 <ethanhuang301@gmail.com>
2022-07-11 17:37:19 +1200

    intial commit

--- background.js
+++ background.js
@@ -6,24 +6,31 @@ const CLOSE_TAB = "CLOSE_TAB";
 const SHOW_BLOCKED_INFO_PAGE = "SHOW_BLOCKED_INFO_PAGE";
 
 const RESOLUTIONS = [
-  CLOSE_TAB,
-  SHOW_BLOCKED_INFO_PAGE,
+	CLOSE_TAB,
+	SHOW_BLOCKED_INFO_PAGE,
 ];
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.local.get(["enabled", "blocked", "resolution"], function (local) {
-    if (typeof local.enabled !== "boolean") {
-      chrome.storage.local.set({ enabled: false });
-    }
 
-    if (!Array.isArray(local.blocked)) {
-      chrome.storage.local.set({ blocked: [] });
-    }
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.storage.local.get(["enabled", "blocked", "resolution"], function(local) {
+		if (typeof local.enabled !== "boolean") {
+			chrome.storage.local.set({
+				enabled: false
+			});
+		}
 
-    if (!RESOLUTIONS.includes(local.resolution)) {
-      chrome.storage.local.set({ resolution: CLOSE_TAB });
-    }
-  });
+		if (!Array.isArray(local.blocked)) {
+			chrome.storage.local.set({
+				blocked: []
+			});
+		}
+
+		if (!RESOLUTIONS.includes(local.resolution)) {
+			chrome.storage.local.set({
+				resolution: CLOSE_TAB
+			});
+		}
+	});
 });
 
 const __removeProtocol = (url) => url.replace(/^http(s?):\/\//, "");
@@ -35,57 +42,190 @@ const __removeTrailingSlash = (url) => url.endsWith("/") ? url.slice(0, -1) : ur
 // "https://music.youtube.com/" => "music.youtube.com"
 // "https://music.youtube.com/explore" => "music.youtube.com/explore"
 const normalizeUrl = (url) => [url]
-  .map(__removeProtocol)
-  .map(__removeWww)
-  .map(__removeTrailingSlash)
-  .pop();
+	.map(__removeProtocol)
+	.map(__removeWww)
+	.map(__removeTrailingSlash)
+	.pop();
 
 // ["youtube.com", "!music.youtube.com"] => [{ path: "music.youtube.com", type: "allow" }, { path: "youtube.com", type: "block" }]
 // ["https://youtube.com/", "!https://music.youtube.com/"] => [{ path: "music.youtube.com", type: "allow" }, { path: "youtube.com", type: "block" }]
 const getRules = (blocked) => {
-  const allowList = blocked
-    .filter((item) => item.startsWith("!"))
-    .map((item) => normalizeUrl(item.substring(1)));
+	const allowList = blocked
+		.filter((item) => item.startsWith("!"))
+		.map((item) => normalizeUrl(item.substring(1)));
 
-  const blockList = blocked
-    .filter((item) => !item.startsWith("!"))
-    .map(normalizeUrl);
+	const blockList = blocked
+		.filter((item) => !item.startsWith("!"))
+		.map(normalizeUrl);
 
-  const rules = [
-    ...allowList.map((path) => ({ path, type: "allow" })),
-    ...blockList.map((path) => ({ path, type: "block" })),
-  ].sort((a, b) => b.path.length - a.path.length); // order the rules by their specificity; the longer the rule, the more specific
+	const rules = [
+		...allowList.map((path) => ({
+			path,
+			type: "allow"
+		})),
+		...blockList.map((path) => ({
+			path,
+			type: "block"
+		})),
+	].sort((a, b) => b.path.length - a.path.length); // order the rules by their specificity; the longer the rule, the more specific
 
-  return rules;
+	return rules;
 };
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-  const url = changeInfo.pendingUrl || changeInfo.url;
-  if (!url || !url.startsWith("http")) {
-    return;
-  }
+var STORED_SCORE = 0;
 
-  const normalizedUrl = normalizeUrl(url);
+function setData(value) {
+	STORED_SCORE = value;
+}
 
-  chrome.storage.local.get(["enabled", "blocked", "resolution"], function (local) {
-    const { enabled, blocked, resolution } = local;
-    if (!enabled || !Array.isArray(blocked) || blocked.length === 0 || !RESOLUTIONS.includes(resolution)) {
-      return;
-    }
+function getData(obj) {
+	chrome.storage.local.get(obj, function(result) {
+		setData(result); // here it works, I can do something with my object
+	});
+	return; // here it doesn't work
+}
 
-    const rules = getRules(blocked);
-    const foundRule = rules.find((rule) => normalizedUrl.startsWith(rule.path) || normalizedUrl.endsWith(rule.path));
-    if (!foundRule || foundRule.type === "allow") {
-      return;
-    }
+function updateScore(url, tabId, subtract_only) {
+	getData("score");
+	const normalizedUrl = normalizeUrl(url);
+	chrome.storage.local.get(["enabled", "blocked", "resolution", "merit_weight", "demerit_weight", "max_point", "score"], function(local) {
+		const {
+			enabled,
+			blocked,
+			resolution,
+			merit_weight,
+			demerit_weight,
+			max_point,
+			score
+		} = local;
 
-    switch (resolution) {
-    case CLOSE_TAB:
-      chrome.tabs.remove(tabId);
-      break;
-    case SHOW_BLOCKED_INFO_PAGE:
-      chrome.tabs.update(tabId, { url: `${chrome.runtime.getURL("blocked.html")}?url=${url}` });
-      break;
-    }
-  });
+		if (!enabled || !Array.isArray(blocked) || blocked.length === 0 || !RESOLUTIONS.includes(resolution)) {
+			if (!subtract_only) {
+				if (score >= parseInt(max_point)) {
+					chrome.storage.local.set({
+						score: parseInt(max_point)
+					});
+				} else {
+					chrome.storage.local.set({
+						score: score + parseInt(merit_weight)
+					});
+				}
+				return;
+			}
+		}
+
+		const rules = getRules(blocked);
+		const foundRule = rules.find((rule) => normalizedUrl.startsWith(rule.path) || normalizedUrl.endsWith(rule.path));
+		if (!foundRule || foundRule.type === "allow") {
+			if (!subtract_only) {
+				if (score >= parseInt(max_point)) {
+					chrome.storage.local.set({
+						score: parseInt(max_point)
+					});
+				} else {
+					chrome.storage.local.set({
+						score: score + parseInt(merit_weight)
+					});
+				}
+				return;
+			}
+		}
+
+		switch (resolution) {
+			case CLOSE_TAB:
+				if (score > 0) {
+					chrome.storage.local.set({
+						score: score - parseInt(demerit_weight)
+					});
+				} else {
+					chrome.tabs.remove(tabId);
+					break;
+				}
+				case SHOW_BLOCKED_INFO_PAGE:
+					if (score > 0) {
+						chrome.storage.local.set({
+							score: score - parseInt(demerit_weight)
+						});
+					} else {
+						chrome.tabs.update(tabId, {
+							url: `${chrome.runtime.getURL("blocked.html")}?url=${url}`
+						});
+						break;
+					}
+		}
+	});
+}
+
+function blocker() {
+	console.log(STORED_SCORE.score);
+
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function(tabs) {
+		var activeTab = tabs[0];
+		var tabId = activeTab.id;
+		var url = activeTab.url
+
+		if (!url || !url.startsWith("http")) {
+			return;
+		}
+
+		updateScore(url, tabId, false);
+	});
+
+	// keep on deducting points even if tab is not active
+	chrome.tabs.query({
+		active: false
+	}, function(tabs) {
+		for (let i = 0; i < tabs.length; i++) {
+			var tab = tabs[i];
+			var tabId = tab.id;
+			var url = tab.url;
+
+			if (!url || !url.startsWith("http")) {
+				continue;
+			}
+
+			updateScore(url, tabId, true);
+		}
+	});
+
+	if (STORED_SCORE.score == null) {
+		chrome.action.setBadgeText({
+			text: "?"
+		});
+	} else {
+		chrome.action.setBadgeText({
+			text: STORED_SCORE.score.toString(10)
+		});
+	}
+
+}
+
+chrome.runtime.onStartup.addListener(function() {
+	chrome.storage.local.get("reset_after_clousure", function(local) {
+		console.log(local.reset_after_clousure);
+		if (local.reset_after_clousure == true) {
+			chrome.storage.local.set({
+				score: 0
+			});
+		}
+	});
 });
+
+chrome.runtime.onInstalled.addListener(function(details) {
+	chrome.storage.local.set({
+		score: 0
+	});
+});
+
+getData("score");
+chrome.action.setBadgeBackgroundColor({
+	color: "#777"
+});
+setInterval(blocker, 1000);
+/*
+chrome.alarms.create("delay", { delayInMinutes: 1/60 });
+chrome.alarms.onAlarm.addListener(blocker);
+*/
--- options.js
+++ options.js
@@ -6,52 +6,116 @@ const blockedList = document.getElementById("blocked-list");
 const resolutionSelect = document.getElementById("resolution-select");
 const enabledToggle = document.getElementById("enabled-toggle");
 
+const meritWeight = document.getElementById("merit-weight");
+const demeritWeight = document.getElementById("demerit-weight");
+const maxPoint = document.getElementById("max-point");
+const resetAfterClousureToggle = document.getElementById("reset-after-clousure-toggle");
+
 blockedList.placeholder = [
-  "facebook.com",
-  "instagram.com",
-  "youtube.com",
-  "!music.youtube.com",
-  "twitter.com",
-  "reddit.com",
-  "!reddit.com/r/MachineLearning",
+	"facebook.com",
+	"instagram.com",
+	"youtube.com",
+	"!music.youtube.com",
+	"twitter.com",
+	"reddit.com",
+	"!reddit.com/r/MachineLearning",
 ].join("\n");
 
 blockedList.addEventListener("change", (event) => {
-  const blocked = event.target.value.split("\n").map(s => s.trim()).filter(Boolean);
+	const blocked = event.target.value.split("\n").map(s => s.trim()).filter(Boolean);
 
-  chrome.storage.local.set({ blocked });
+	chrome.storage.local.set({
+		blocked
+	});
 });
 
 resolutionSelect.addEventListener("change", (event) => {
-  const resolution = event.target.value;
+	const resolution = event.target.value;
 
-  chrome.storage.local.set({ resolution });
+	chrome.storage.local.set({
+		resolution
+	});
 });
 
 enabledToggle.addEventListener("change", (event) => {
-  const enabled = event.target.checked;
+	const enabled = event.target.checked;
 
-  chrome.storage.local.set({ enabled });
+	chrome.storage.local.set({
+		enabled
+	});
+});
+
+meritWeight.addEventListener("change", (event) => {
+	const merit_weight = event.target.value;
+
+	chrome.storage.local.set({
+		merit_weight
+	});
+});
+
+demeritWeight.addEventListener("change", (event) => {
+	const demerit_weight = event.target.value;
+
+	chrome.storage.local.set({
+		demerit_weight
+	});
+});
+
+maxPoint.addEventListener("change", (event) => {
+	const max_point = event.target.value;
+
+	chrome.storage.local.set({
+		max_point
+	});
+});
+
+resetAfterClousureToggle.addEventListener("change", (event) => {
+	const reset_after_clousure = event.target.checked;
+
+	chrome.storage.local.set({
+		reset_after_clousure
+	});
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.local.get(["enabled", "blocked", "resolution"], function (local) {
-    const { enabled, blocked, resolution } = local;
-    if (!Array.isArray(blocked)) {
-      return;
-    }
+	chrome.storage.local.get(["enabled", "blocked", "resolution", "merit_weight", "demerit_weight", "max_point", "reset_after_clousure"], function(local) {
+		const {
+			enabled,
+			blocked,
+			resolution,
+			merit_weight,
+			demerit_weight,
+			max_point,
+			reset_after_clousure
+		} = local;
 
-    // blocked
-    var value = blocked.join("\r\n"); // display every blocked in new line
-    blockedList.value = value;
+		if (!Array.isArray(blocked)) {
+			return;
+		}
 
-    // resolution
-    resolutionSelect.value = resolution;
+		// blocked
+		var value = blocked.join("\r\n"); // display every blocked in new line
+		blockedList.value = value;
 
-    // enabled
-    enabledToggle.checked = enabled;
+		// resolution
+		resolutionSelect.value = resolution;
 
-    // UI ready
-    document.body.classList.add("ready");
-  });
+		// enabled
+		enabledToggle.checked = enabled;
+
+		// meritWeight
+		meritWeight.value = merit_weight;
+
+		// demeritWeight
+		demeritWeight.value = demerit_weight;
+
+		// maxPoint
+		maxPoint.value = max_point;
+
+		// resetAfterClousureToggle
+		resetAfterClousureToggle.checked = reset_after_clousure;
+
+		// UI ready
+		document.body.classList.add("ready");
+	});
 });
